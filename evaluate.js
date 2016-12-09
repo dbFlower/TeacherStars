351268650dbeebecc743141dd5dd6ff82e9b8da3
dbFlower <anna2011@vip.qq.com>
2016-12-09 18:17:17 +0800

    Bug Fix
    监听iframe onload 事件而不是单纯的定时

--- evaluate.js
+++ evaluate.js
@@ -1,20 +1,32 @@
-// evaluate teachers.
-// you can pase your rate and delay as parameters.
-(function(rate, delay){
-  var len = window.frames[0].document.querySelectorAll('#pjkc option').length;
-  rate = rate || 1, delay = delay || 1000;
-  var _delay = 0
-  for(var i = 0 ; i < len; ++i){
-    setTimeout(function(){
-      var j = 0;
-      Array.apply(this, window.frames[0].document.querySelectorAll('.datelist select')).forEach(function(a){
-        a.querySelectorAll('option')[rate].selected = true;
-        if( ++j % 9 == 0){a.querySelectorAll('option')[rate == 2 ? 1 : 2].selected = true;}
-      });
-      window.frames[0].document.querySelector('#Button1').click();
-    }, (_delay += delay));
-    }
-
+(function(rate){
+  var frame = document.getElementById('iframeautoheight')
+  var length = frame.contentDocument.querySelectorAll('#pjkc option').length
+  rate = rate || 1
+  var i = 1
+  var _onload = frame.onload 
+  frame.onload = function (events) {
+	try {
+	  _onload.apply(this, arguments)
+	} catch (e) {}
+	if (i < length) {
+	  setRate()
+	  ++ i
+	} else {
+	// frame.contentDocument.querySelector('#Button2').click()
+	}
   }
-)(1, 1000);
+  
+  setRate()
+  
+  function setRate () {
+	var j = 0
+	Array.apply([], frame.contentDocument.querySelectorAll('.datelist select')).forEach(function(a){
+	  a.querySelectorAll('option')[rate].selected = true
+	  if ( ++j % 9 == 0) {
+	    a.querySelectorAll('option')[rate == 2 ? 1 : 2].selected = true
+	  }
+	});
+	frame.contentDocument.querySelector('#Button1').click()
+  }
 
+})(1)
